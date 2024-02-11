f6e7c8b2d81ef493ec0401cf2d6c64ca68cb5474
irondvst <159679573+irondvst@users.noreply.github.com>
2024-02-11 04:17:59 -0500

    discord formatting

--- find_star/_constants/discord.py
+++ find_star/_constants/discord.py
@@ -14,7 +14,7 @@ STARHUNTER_ROLE_ID = getenv("STARHUNTER_ROLE_ID", "StarHunters")
 
 DISCORD_MESSAGE_SPEC = """
 **T{tier:d} Star** at **{loc:s}** <@&%(role_id)s> 
-> ~<t:{time:d}:R> by {scout:s} on [osrsportal.com](https://osrsportal.com/shooting-stars-tracker)
+> ~<t:{time:d}:R> by {scout:s} on [osrsportal](https://osrsportal.com/shooting-stars-tracker)
 """.strip() % {
     "role_id": STARHUNTER_ROLE_ID
 }
